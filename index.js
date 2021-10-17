a5afc90605221368fbb7f26ffbafbf86c961b708
Kush19974 <kushpatel19974@gmail.com>
2021-10-17 21:52:01 +0530

    Project structure changes.

--- index.js
+++ index.js
@@ -1,20 +1,10 @@
 const express = require('express');
-const router = require('./router');
+const router = require('./router/router');
 
 let app = express();
 
 app.use(express.json());
 app.use(router);
-
-// get request
-app.get('/', (req, res) => {
-    if(req.url === '/')
-        res.send(`Welcome to express app`);
-});
-
-app.all('*', (req, res) => {
-    res.status(404).send(`<h3> Resource not found<h3>`)
-});
 
 const PORT = process.env.PORT || 9100;
 
--- router/router.js
+++ router/router.js
@@ -1,7 +1,7 @@
 let router = require('express').Router();
-let tickerTape = require('./ticker').ticker;
-let orderExecuted = require('./ticker').orderExecuted;
-let exectuedOrderApi = require('./api').exectuedOrder;
+let tickerTape = require('../data/ticker').ticker;
+let orderExecuted = require('../data/ticker').orderExecuted;
+let exectuedOrderApi = require('../apis/exectuedOrder').exectuedOrder;
 
 router.route('/add-new-order')
     .post(function (req, res) {
